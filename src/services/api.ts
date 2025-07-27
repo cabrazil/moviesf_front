f144e142382d131dd7f6de1f6227dc22d1ef13fe
cabrazil <cabrazil@gmail.com>
2025-07-27 16:16:59 -0300

    api new url vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Movie } from '../types';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://172.19.18.160:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://moviesf-back.vercel.app',
 });
 
 export interface MainSentiment {
