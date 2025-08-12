acaeacb6fc1d67971f9d509b8c15a2f795048343
cabrazil <cabrazil@gmail.com>
2025-08-12 13:23:47 -0300

    Fix App.tsx: remove MovieDetailWrapper import and use MovieDetailsPage directly

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import Home from './pages/Home';
 import JourneyIntro from './pages/JourneyIntro';
 import MovieSuggestionsPage from './pages/MovieSuggestionsPage';
 import MovieSuggestionsPageMinimal from './pages/MovieSuggestionsPageMinimal';
-
 import StreamingFilters from './pages/StreamingFilters';
-import MovieDetailWrapper from './pages/MovieDetailWrapper';
+import MovieDetailsPage from './pages/MovieDetailsPage';
 
 
 function App() {
@@ -20,8 +19,7 @@ function App() {
           <Route path="/sugestoes" element={<MovieSuggestionsPage />} />
           <Route path="/sugestoes/minimal" element={<MovieSuggestionsPageMinimal />} />
           <Route path="/suggestions" element={<MovieSuggestionsPageMinimal />} />
-          <Route path="/filme/:id" element={<MovieDetailWrapper />} />
-          <Route path="/filme/:slug" element={<MovieDetailWrapper />} />
+          <Route path="/filme/:id" element={<MovieDetailsPage />} />
         </Routes>
       </Router>
     </ThemeProviderWrapper>
