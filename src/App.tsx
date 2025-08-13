64d49dad40863828dddc9e797b517c0cc7dcaed0
cabrazil <cabrazil@gmail.com>
2025-08-13 20:45:24 -0300

    new plataforms logo2

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import JourneyIntro from './pages/JourneyIntro';
 import MovieSuggestionsPage from './pages/MovieSuggestionsPage';
 import MovieSuggestionsPageMinimal from './pages/MovieSuggestionsPageMinimal';
 import StreamingFilters from './pages/StreamingFilters';
-import MovieDetailsPage from './pages/MovieDetailsPage';
-import MovieDetailWrapper from './pages/MovieDetailWrapper';
+
 
 
 function App() {
@@ -20,8 +19,7 @@ function App() {
           <Route path="/sugestoes" element={<MovieSuggestionsPage />} />
           <Route path="/sugestoes/minimal" element={<MovieSuggestionsPageMinimal />} />
           <Route path="/suggestions" element={<MovieSuggestionsPageMinimal />} />
-          <Route path="/filme/:id" element={<MovieDetailWrapper />} />
-          <Route path="/filme/:slug" element={<MovieDetailWrapper />} />
+
         </Routes>
       </Router>
     </ThemeProviderWrapper>
