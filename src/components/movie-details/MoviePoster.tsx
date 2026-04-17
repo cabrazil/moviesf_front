131638e8d4f7171f7b0a3d958fe90f8e0416f5db
cabrazil <cabrazil@gmail.com>
2026-04-17 19:09:07 -0300

    Fix: Remove unused imports to resolve build errors on VPS

--- src/components/movie-details/MoviePoster.tsx
+++ src/components/movie-details/MoviePoster.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Paper, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { getBlogImageUrl } from '../../utils/blogImages';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
